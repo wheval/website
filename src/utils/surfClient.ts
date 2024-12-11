e38ff8985222daedae04495ad5169d7809fecef1
YepW <jeffier2015@gmail.com>
2024-12-11 10:06:55 +0800

    delete surf

--- src/utils/surfClient.ts
+++
@@ -1,8 +0,0 @@
-import { createSurfClient } from "@thalalabs/surf";
-import { aptosClient } from "./aptosClient";
-
-const surf = createSurfClient(aptosClient());
-
-export function surfClient() {
-  return surf;
-}
