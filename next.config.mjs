bdd71bb235dc8f33f932a403e65d53b1aafdce62
YepW <jeffier2015@gmail.com>
2024-12-11 16:12:02 +0800

    chore

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,19 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async headers() {
+    return [
+      {
+        source: "/api/chat",
+        headers: [
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "POST, GET, OPTIONS",
+          },
+        ],
+      },
+    ];
+  },
   output: "export", // Outputs a Single-Page Application (SPA).
   distDir: "./dist", // Changes the build output directory to `./dist/`.
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
