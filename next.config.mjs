0b375bf0774d9fde1b77faf3d4f99c2a50c1329b
YepW <jeffier2015@gmail.com>
2024-12-11 12:33:12 +0800

    chore: delete pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,10 @@
-import withPWAInit from "@ducanh2912/next-pwa";
-
-const withPWA = withPWAInit({
-  disable: false,
-});
-
-// Your Next config is automatically typed!
-export default withPWA({
+const nextConfig = {
   images: {
     unoptimized: true,
   },
   output: "export", // Outputs a Single-Page Application (SPA).
   distDir: "./dist", // Changes the build output directory to `./dist/`.
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH, // Sets the base path to `/some-base-path`.
-});
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+};
+
+export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import type { ReactNode } from "react";
 import { irishGrover } from "@/fonts/fonts";
-import Head from "next/head";
 
 import { ReactQueryProvider } from "@/components/ReactQueryProvider";
 import { WalletProvider } from "@/components/WalletProvider";
@@ -15,22 +14,19 @@ export const metadata: Metadata = {
   title: "Art3MisOracle",
   description: "Art3MisOracle",
   manifest: "/manifest.json",
+  openGraph: {
+    title: "Art3MisOracle",
+    description:
+      "🧚‍♀️ Mystical #AITarot on the #Blockchain, in the heart of the #cryptoverse ✨",
+    images: ["/favicon.ico"],
+    url: "https://www.art3mis.xyz/",
+    type: "website",
+  },
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta property="og:title" content="Art3MisOracle" />
-        <meta
-          property="og:description"
-          content="🧚‍♀️ Mystical #AITarot on the #Blockchain, in the heart of the #cryptoverse ✨"
-        />
-        <meta property="og:image" content="/favicon.ico" />
-        <meta property="og:url" content="https://www.art3mis.xyz/" />
-        <meta property="og:type" content="website" />
-      </Head>
       <html lang="en" className={`${irishGrover.variable}`}>
         <body>
           <WalletProvider>
